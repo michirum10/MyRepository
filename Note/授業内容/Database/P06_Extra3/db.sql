ec11f0698ec6121a52b6b9ee1fb766db0d2710df
michirum10 <0037.mori.michiru@gmail.com>
2024-07-05 01:59:40 +0900

    家で変更してgoogleと同期

--- Note/授業内容/Database/P06_Extra3/db.sql
+++ Note/授業内容/Database/P06_Extra3/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE
-    user_table (
+    user (
         id INTEGER,
         name TEXT,
         email TEXT,
@@ -7,31 +7,31 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    follow_table (
+    follow (
         id INTEGER,
         followerId INTEGER
     );
 
--- DROP TABLE user_table;
--- DROP TABLE follow_table;
+-- DROP TABLE user;
+-- DROP TABLE follow;
 
-INSERT INTO user_table VALUES(1,'もっくん','mokkun@example.com',19);
-INSERT INTO user_table VALUES(2,'みみこ','mimiko@example.net',20);
-INSERT INTO user_table VALUES(3,'さくら','sakura@example.com',31);
-INSERT INTO user_table VALUES(4,'ひよこ','hiyoko@example1.jp',23);
-INSERT INTO user_table VALUES(5,'すずき','suzuki@example.jp',28);
+INSERT INTO user VALUES(1,'もっくん','mokkun@example.com',19);
+INSERT INTO user VALUES(2,'みみこ','mimiko@example.net',20);
+INSERT INTO user VALUES(3,'さくら','sakura@example.com',31);
+INSERT INTO user VALUES(4,'ひよこ','hiyoko@example1.jp',23);
+INSERT INTO user VALUES(5,'すずき','suzuki@example.jp',28);
 
-INSERT INTO follow_table VALUES(1,2);
-INSERT INTO follow_table VALUES(1,3);
-INSERT INTO follow_table VALUES(1,4);
-INSERT INTO follow_table VALUES(1,5);
-INSERT INTO follow_table VALUES(3,1);
-INSERT INTO follow_table VALUES(3,2);
-INSERT INTO follow_table VALUES(4,5);
-INSERT INTO follow_table VALUES(5,1);
-INSERT INTO follow_table VALUES(5,2);
-INSERT INTO follow_table VALUES(5,3);
-INSERT INTO follow_table VALUES(5,4);
+INSERT INTO follow VALUES(1,2);
+INSERT INTO follow VALUES(1,3);
+INSERT INTO follow VALUES(1,4);
+INSERT INTO follow VALUES(1,5);
+INSERT INTO follow VALUES(3,1);
+INSERT INTO follow VALUES(3,2);
+INSERT INTO follow VALUES(4,5);
+INSERT INTO follow VALUES(5,1);
+INSERT INTO follow VALUES(5,2);
+INSERT INTO follow VALUES(5,3);
+INSERT INTO follow VALUES(5,4);
 
 -- ユーザーテーブル
 -- ID,ユーザー,メール,年齢
@@ -57,42 +57,50 @@ INSERT INTO follow_table VALUES(5,4);
 
 -- 誰もフォローしていないユーザーの名前を表示せよ。
 SELECT
-    user_table.name AS フォローなし
+    user.name AS フォローなし
 FROM
-    user_table
+    user
 LEFT JOIN -- フォローしていないユーザーも含める
-    follow_table ON user_table.id = follow_table.id
+    follow ON user.id = follow.id
 WHERE -- NULLかどうかチェックして表示
-    follow_table.id IS NULL;
+    follow.id IS NULL;
 
 -- 別解
 -- 誰もフォローしていないユーザーの名前を表示せよ。
 SELECT 
-    user_table.name, 
-    COUNT(follow_table.id) count
+    user.name, 
+    COUNT(follow.id) count
 FROM 
-    user_table
+    user
 LEFT JOIN 
-    follow_table ON user_table.id = follow_table.id
+    follow ON user.id = follow.id
 GROUP BY 
-    user_table.name 
+    user.name 
 HAVING 
     count = 0;
 
 -- 10代、20代、30代といった年代別にフォロー数の平均を表示せよ。
 SELECT
-    (age / 10) * 10 || '代' AS 年代, -- 各年代を計算して「年代」と置く
+    (age / 10) * 10 AS 年代, -- 各年代を計算して「年代」と置く
     AVG( -- 平均を計算
         (SELECT COUNT(*)  -- フォロー数を計算
-         FROM follow_table 
-         WHERE follow_table.id = user_table.id)
+         FROM follow 
+         WHERE follow.id = user.id)
     ) AS 平均フォロー数
 FROM
-    user_table
+    user
 GROUP BY
     年代
 ORDER BY
     年代;
+
+-- 先生のやつ平均年齢計算してる？
+-- SELECT user.age / 10 AS avgGroup, AVG(user.age) 平均年齢 FROM 
+-- (SELECT user.name, COUNT(follow.id) count, age
+-- FROM user
+-- LEFT JOIN follow ON user.ID = follow.id
+-- GROUP BY user.name) user
+-- GROUP BY avgGroup;
 
 -- 相互フォローしているユーザーのIDを表示せよ。
 -- なお、重複は許さないものとする。
@@ -101,9 +109,9 @@ SELECT DISTINCT
     A.id AS ユーザー1, -- Aテーブルから取得して表示
     A.followerId AS ユーザー2 -- Aテーブルから取得して表示
 FROM
-    follow_table AS A -- テーブルにキーワードを付与(A)
+    follow AS A -- テーブルにキーワードを付与(A)
 INNER JOIN -- 自己結合
-    follow_table AS B -- テーブルにキーワードを付与(B)
+    follow AS B -- テーブルにキーワードを付与(B)
 ON -- 相互フォローを調べる
     A.id = B.followerId AND A.followerId = B.id
 WHERE -- 各ペアを一度だけ表示(絞り込み)
@@ -140,8 +148,10 @@ ORDER BY
 -- left join Flower as B
 -- on A.family_ID = B.flower_ID;
 
--- SELECT * FROM follow_table;全表示
+-- SELECT * FROM follow;全表示
 
 いま変更しました。7/4 16:17
 こっちがメイン
+
+家で変更しました。7/5 01:37
 
